7ee253f6c3075136c59f205ae8813e6974f5289e
vaibhavhvr <67812827+vaibhavhvr@users.noreply.github.com>
2023-06-05 19:25:36 +0530

    parse the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,7 @@ fuits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fuits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
