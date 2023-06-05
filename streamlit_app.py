c7e31b7fe814ed243fd9902e61513d1f1575af1f
vaibhavhvr <67812827+vaibhavhvr@users.noreply.github.com>
2023-06-05 19:31:46 +0530

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just writes the json data to the screen
 
 # Normalize the json reponse
