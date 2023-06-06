7779baf18d14f11ecccec20c970b6c8792ee8396
vaibhavhvr <67812827+vaibhavhvr@users.noreply.github.com>
2023-06-06 18:28:38 +0530

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fuits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return sfruityvice_normalized
+  return fruityvice_normalized
 
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
